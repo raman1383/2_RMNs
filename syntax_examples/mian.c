d7352564bc964048399b1968ea2c29d9df7138ad
raman1383 <raman83shariati@gmail.com>
2022-08-07 17:58:45 +0430

    s

---
+++ syntax_examples/mian.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    // data types
+    int age = 32;
+    bool isAllowed = false;
+
+    // control flow
+    if (age >= 18)
+    {
+        printf("drink !");
+    }
+    else if (isAllowed == false)
+    {
+        printf("Do not drink !!");
+    }
+    else
+    {
+        printf("Do not give a rats ass");
+    }
+
+    // loops
+
+    return 0;
+}
+
+// functions
