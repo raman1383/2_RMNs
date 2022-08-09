a861b4f3708cf5b4bc7ef8b30544da47e98feb25
raman1383 <raman83shariati@gmail.com>
2022-08-09 17:21:30 +0430

    r

--- syntax_examples/main.js
+++ syntax_examples/main.js
@@ -1,5 +1,23 @@
 console.log("Hello from javascript");
 
-if (true) {
+let x = 12;
+const e = 1.6e19;
+let isAlive = true;
+let fname = undefined;
+let lname = null;
 
+const value1 = Symbol('hello');
+console.log(value1.description);
+
+let id = Symbol("id");
+
+let person = {
+    name: "Jack",
+    age: 25,
+    [id]: 12
+};
+
+// using for...in
+for (let key in person) {
+    console.log(key);
 }
